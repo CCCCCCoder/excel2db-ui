ff61f3828820d736e03b32a8f4d7d368e2825b98
Manley_li <manley_li@outlook.com>
2021-12-28 23:04:03 +0800

    excel uploader component

---
+++ src/components/ExcelUploader.vue
@@ -0,0 +1,25 @@
+<template>
+  <el-upload
+      :before-remove="beforeRemove"
+      :file-list="fileList"
+      :limit="3"
+      :multiple="false"
+      :on-exceed="handleExceed"
+      :on-preview="handlePreview"
+      :on-remove="handleRemove"
+      action="/api/excel/upload"
+      class="upload-demo">
+    <el-button size="small" type="primary">点击上传</el-button>
+    <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
+  </el-upload>
+</template>
+
+<script>
+export default {
+  name: "ExcelUploader"
+}
+</script>
+
+<style scoped>
+
+</style>
